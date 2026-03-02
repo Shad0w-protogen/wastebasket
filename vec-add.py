9bdba137ee1d699a840d10d7abf7384319db1ef8
Shad0w-protogen <nosok.keramicheskiy@mail.ru>
2026-03-02 20:19:38 +0300

    Fixed version

--- vec-add.py
+++ vec-add.py
@@ -1,8 +1,8 @@
 def Vector_addition(a,b):
     return a[0]+b[0],a[1]+b[1]
 
-vector1 = map(int, input().split)
-vector2 = (-1,0)
+vector1 = (int(input()),int(input()))
+vector2 = (int(input()),int(input()))
 
 vector_lol = Vector_addition(vector1,vector2)
 print(vector_lol)
